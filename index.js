c4e5937e3256ca49d7eb78a7bd1f0fe3b6094cca
zapravkaforpods <161873324+zapravkaforpods@users.noreply.github.com>
2024-03-10 00:28:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ burgerIcon.addEventListener('click', () => {
     menuItems.classList.toggle('visible');
     burgerIcon.textContent = menuItems.classList.contains('visible') ? '❎' : '㊂'; // Зміна тексту іконки при відкритті/закритті меню
 });
+
+const toggleButton = document.getElementById('mode-toggle');
+const body = document.body;
+body.classList.add('light-mode');
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+});
+
+
+const modeToggle = document.getElementById('mode-toggle');
+
+modeToggle.addEventListener('click', function() {
+    if (modeToggle.textContent === '🅞🅝') {
+        modeToggle.textContent = '🅞🅕🅕'; 
+    }
+     else {
+        modeToggle.textContent = '🅞🅝'; 
+    }
+});
+
+
