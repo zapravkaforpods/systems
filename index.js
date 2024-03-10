76aba0efba2ecca9ba4ffba625200e865869f38f
zapravkaforpods <161873324+zapravkaforpods@users.noreply.github.com>
2024-03-10 13:55:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ const menuItems = document.querySelector('.menu-items');
 
 burgerIcon.addEventListener('click', () => {
     menuItems.classList.toggle('visible');
-    burgerIcon.textContent = menuItems.classList.contains('visible') ? '❎' : '㊂'; // Зміна тексту іконки при відкритті/закритті меню
+    burgerIcon.textContent = menuItems.classList.contains('visible') ? '❎' : '㊂'; 
 });
 
-//////////////
 const modeToggle = document.getElementById('mode-toggle');
 
-// Перевіряємо, чи є попередній стан кнопки в localStorage
+
 if (localStorage.getItem('backgroundChanged') === 'true') {
-    modeToggle.style.backgroundColor = 'black'; // Змінити фон кнопки на чорний колір
+    modeToggle.style.backgroundColor = 'black'; 
 }
-//////////////
+
 modeToggle.addEventListener('click', function() {
     if (modeToggle.textContent === '🅞🅝') {
         modeToggle.textContent = '🅞🅕🅕';
-        modeToggle.style.backgroundColor = 'black'; // Змінити фон кнопки на чорний колір
-        localStorage.setItem('backgroundChanged', 'true'); // Зберігаємо стан фону в localStorage
+        modeToggle.style.backgroundColor = 'black'; 
+        localStorage.setItem('backgroundChanged', 'true'); 
     } else {
         modeToggle.textContent = '🅞🅝';
-        modeToggle.style.backgroundColor = '#f73636'; // Повернути початковий фон кнопки
-        localStorage.setItem('backgroundChanged', 'false'); // Зберігаємо стан фону в localStorage
+        modeToggle.style.backgroundColor = '#f73636'; 
+        localStorage.setItem('backgroundChanged', 'false'); 
     }
 });
