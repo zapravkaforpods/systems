e95b5451b26157dd0ecb4354280b11c6cb4bee00
zapravkaforpods <161873324+zapravkaforpods@users.noreply.github.com>
2024-03-16 13:56:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,5 +50,42 @@ const image = document.querySelector('.image');
 window.addEventListener('resize', () => {
   image.width = window.innerWidth;
 });
+//////////////
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.order-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
+//////////////
+
+function copyToClipboard() {
+    const contentToCopy = document.getElementById('content');
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    range.selectNodeContents(contentToCopy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    document.execCommand('copy');
+    alert('Контент скопійовано: ' + contentToCopy.innerText);
+    selection.removeAllRanges();
+}
 
 
